12114ec96fd45b142193e9bd7c4e3a03c563a0c8
saadbaig07 <mdsaadbaig07@gmail.com>
2026-07-06 10:31:48 +0530

    updated backend/src/controller/auth.controller

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,7 +1,7 @@
-const userModel = require("../models/user.model")
-const bcrypt = require("bcryptjs")
-const jwt = require("jsonwebtoken")
-const tokenblacklistmodel=require("../models/blacklist.model")
+const userModel = require("../models/user.model");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const tokenblacklistmodel = require("../models/blacklist.model");
 
 /**
  * @name registerUserController
@@ -9,45 +9,55 @@ const tokenblacklistmodel=require("../models/blacklist.model")
  * @access Public
  */
 async function registerUserController(req, res) {
-    const { username, email, password } = req.body
+    const { username, email, password } = req.body;
 
     if (!username || !email || !password) {
         return res.status(400).json({
             message: "PLEASE PROVIDE ALL USERNAME, EMAIL and PASSWORD"
-        })
+        });
     }
 
     const isUserExist = await userModel.findOne({
         $or: [{ username }, { email }]
-    })
+    });
 
     if (isUserExist) {
         if (isUserExist.username === username) {
             return res.status(400).json({
                 message: "ACCOUNT ALREADY EXIST WITH THIS USERNAME"
-            })
+            });
         }
 
         return res.status(400).json({
             message: "ACCOUNT ALREADY EXIST WITH THIS EMAIL"
-        })
+        });
     }
 
-    const hash = await bcrypt.hash(password, 10)
+    const hash = await bcrypt.hash(password, 10);
 
     const user = await userModel.create({
         username,
         email,
         password: hash
-    })
+    });
 
     const token = jwt.sign(
-        { id: user._id, username: user.username },
+        {
+            id: user._id,
+            username: user.username
+        },
         process.env.JWT_SECRET,
-        { expiresIn: "1d" }
-    )
+        {
+            expiresIn: "500d"
+        }
+    );
 
-    res.cookie("token", token)
+    res.cookie("token", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+        maxAge: 24 * 60 * 60 * 1000
+    });
 
     res.status(201).json({
         message: "USER REGISTERED SUCCESSFULLY",
@@ -56,77 +66,97 @@ async function registerUserController(req, res) {
             username: user.username,
             email: user.email
         }
-    })
+    });
 }
 
 /**
  * @name authLoginController
- * @description to login user
- * @access public
+ * @description Login User
+ * @access Public
  */
 async function userLoginController(req, res) {
-    //console.log("LOGIN CONTROLLER HIT");
-        const {email,password}=req.body
-        const user = await userModel.findOne({email})
-        if(!user)
-            return res.status(400).json({
-                message:"invalid user email or password"
-        })
-        const isValidPassword= await bcrypt.compare(password,user.password)
-        if(!isValidPassword)
-            return res.status(400).json({
-                message:"invalid user email or password"
-        })
-        const token = jwt.sign(
-        { id: user._id, username: user.username },
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+        return res.status(400).json({
+            message: "Invalid email or password"
+        });
+    }
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+
+    if (!isValidPassword) {
+        return res.status(400).json({
+            message: "Invalid email or password"
+        });
+    }
+
+    const token = jwt.sign(
+        {
+            id: user._id,
+            username: user.username
+        },
         process.env.JWT_SECRET,
-        { expiresIn: "1d" }
-    )
-    res.cookie("token",token)
+        {
+            expiresIn: "1d"
+        }
+    );
+
+    res.cookie("token", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+        maxAge: 24 * 60 * 60 * 1000
+    });
+
     res.status(200).json({
-        message:"USER LOGIN SUCCESSFULLY",
-        user:{
+        message: "USER LOGIN SUCCESSFULLY",
+        user: {
             id: user._id,
             username: user.username,
             email: user.email
         }
-    })  
-    };
-
-    //  throw new Error("TEST ERROR");
-
-/**
- * @name logoutusercontroller
- * @description remove token from cookie and add token in blacklist
- * @access public
- */
-async function logoutusercontroller(req,res){
-    const token = req.cookies.token
-    if(token){
-        await tokenblacklistmodel.create({token})
-    }
-    res.clearCookie("token")
-    res.status(200).json({
-        message:"USER LOGGED OUT SUCESSFULLY"
-    })
+    });
 }
 
 /**
- * @name get-me
- * @description get the current login user details
- * @access private
+ * @name logoutUserController
+ * @description Logout User
  */
-async function getMeController(req,res){
-    const user = await userModel.findById(req.user.id)
+async function logoutusercontroller(req, res) {
+    const token = req.cookies.token;
+
+    if (token) {
+        await tokenblacklistmodel.create({ token });
+    }
+
+    res.clearCookie("token", {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none"
+    });
 
     res.status(200).json({
-        message:"user detail fetched successfully",
-        user:{
-            id:user._id,
-            username:user.username,
-            email:user.email
+        message: "USER LOGGED OUT SUCCESSFULLY"
+    });
+}
+
+/**
+ * @name getMeController
+ */
+async function getMeController(req, res) {
+    const user = await userModel.findById(req.user.id);
+
+    res.status(200).json({
+        message: "User details fetched successfully",
+        user: {
+            id: user._id,
+            username: user.username,
+            email: user.email
         }
-    })
+    });
 }
 
 module.exports = {
@@ -134,4 +164,4 @@ module.exports = {
     userLoginController,
     logoutusercontroller,
     getMeController
-}
+};
