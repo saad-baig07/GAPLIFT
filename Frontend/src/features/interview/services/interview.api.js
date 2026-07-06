6ae43ea54a788d202a6a83b5aa3a13584bff7ead
saadbaig07 <mdsaadbaig07@gmail.com>
2026-07-06 09:39:48 +0530

    Updated authentication and interview APIs

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const interviewApi = axios.create({
-  baseURL: "http://localhost:3000/api/interview",
+  baseURL: "https://gaplift-1.onrender.com/api/interview",
   withCredentials: true
 })
 
